572665c963898545f7cc9fa03b57d5ffa4e2a165
Victor Rubenko <vrubenko@gmail.com>
2022-02-11 22:28:00 +0300

    add game functionality

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -8,12 +8,83 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    var score: Int = 0
+    var round: Int = 0
+    
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var targetValueLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var roundLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startNewGame()
     }
-
+    
+    @IBAction func showAlert() {
+        let difference = abs(currentValue - targetValue)
+        var points = 100 - difference
+        
+        score += points
+        
+        let title: String
+        switch difference {
+        case 0:
+            title = "Perfect!"
+            points += 100
+        case 1...5:
+            title = "You almost had it!"
+            points += 50
+        case 6...10:
+            title = "Pretty good!"
+        default:
+            title = "Not even close..."
+        }
+        let message = "You scored \(points) points"
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        
+        let action = UIAlertAction(title: "OK", style: .default, handler: { _ in
+            self.startNewRound()
+        })
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
+    
+    func startNewRound() {
+        targetValue = Int.random(in: 0...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        round += 1
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        targetValueLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
+    
+    @IBAction func startOver() {
+        startNewGame()
+    }
+    
+    func startNewGame() {
+        score = 0
+        round = 0
+        startNewRound()
+    }
 
 }
 
