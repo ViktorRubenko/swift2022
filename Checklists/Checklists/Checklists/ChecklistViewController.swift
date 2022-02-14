0031dc5134601c29e901162108892f9af1143162
Victor Rubenko <vrubenko@gmail.com>
2022-02-14 19:56:26 +0300

    init Checklists app

---
+++ Checklists/Checklists/Checklists/ChecklistViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Victor Rubenko on 14.02.2022.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Table View Data Source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        return cell
+    }
+
+
+}
+
