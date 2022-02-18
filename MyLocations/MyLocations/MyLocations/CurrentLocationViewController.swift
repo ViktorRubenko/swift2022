9b0fe216e1442e24d1d3f3de606dbcc83ceb4fe9
Victor Rubenko <vrubenko@gmail.com>
2022-02-19 00:58:39 +0300

    init MyLocations

---
+++ MyLocations/MyLocations/MyLocations/CurrentLocationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLocations
+//
+//  Created by Victor Rubenko on 19.02.2022.
+//
+
+import UIKit
+
+class CurrentLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
