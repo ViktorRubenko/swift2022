aff10bffceb46daa673c8c9de138de8f2de25e2b
Victor Rubenko <vrubenko@gmail.com>
2022-03-06 04:31:23 +0300

    Fix HeaderCell selectionStyle

--- WeatherApp/WeatherApp/ViewControllers/Views/HeaderTableViewCell.swift
+++ WeatherApp/WeatherApp/ViewControllers/Views/HeaderTableViewCell.swift
@@ -22,6 +22,7 @@ class HeaderTableViewCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
+        selectionStyle = .none
         setupViews()
     }
     
